e10ae661b678538fa4fb60cd5b7de85d14afa489
Wavell Watson <wavell.watson@gmail.com>
2013-12-06 19:45:22 -0600

    comments

--- lib/elixir_otp_koans/koans_server.ex
+++ lib/elixir_otp_koans/koans_server.ex
@@ -1,53 +1,53 @@
 ## test 1 make a module
-defmodule ElixirOtpKoans.KoansServer do
-
-  # test 6
-  defrecord State, veggies: nil
-
-  # test 2 make a gen server
-  use GenServer.Behaviour
-
-  # test 3
-  def start_link do
-    :gen_server.start_link({:local, __MODULE__}, __MODULE__, [], [])
-  end
-
-  # test 4, test 4.a
-  def handle_cast(:stop, state) do
-    IO.puts "DEBUG: hand_cast :stop"
-    {:stop, :normal, state}
-  end
-
-  # test 4
-  def stop(pid) do
-    :gen_server.cast(pid, :stop)
-  end 
-
- # test 5
-  def start_link(state) do
-    :gen_server.start_link({:local, __MODULE__}, __MODULE__, [state], [])
-  end
-
- # test 6
-  def init([]) do
-    state = State.new
-    {:ok, state}
-  end
-  
-  # test 6
-  def init([veggies]) do
-   state = State.new(veggies: veggies)
-   {:ok, state}
-  end
-
-  # test 7
-  def handle_call(:fetch, _from, state) do
-    {:reply, state, state}
-  end
-
-  # test 8 
-  def fetch(pid) do
-    ret = :gen_server.call(pid, :fetch)
-  end
-
-end
+#defmodule ElixirOtpKoans.KoansServer do
+#
+#  # test 6
+#  defrecord State, veggies: nil
+#
+#  # test 2 make a gen server
+#    use GenServer.Behaviour
+#
+#  # test 3
+#  def start_link do
+#    :gen_server.start_link({:local, __MODULE__}, __MODULE__, [], [])
+#  end
+#
+#   # test 4, test 4.a
+#   def handle_cast(:stop, state) do
+#     IO.puts "DEBUG: hand_cast :stop"
+#     {:stop, :normal, state}
+#   end
+#  
+#    # test 4
+#    def stop(pid) do
+#      :gen_server.cast(pid, :stop)
+#    end 
+#
+# # test 5
+#  def start_link(state) do
+#    :gen_server.start_link({:local, __MODULE__}, __MODULE__, [state], [])
+#  end
+#
+# # test 6
+#  def init([]) do
+#    state = State.new
+#    {:ok, state}
+#  end
+#  
+#  # test 6
+#  def init([veggies]) do
+#   state = State.new(veggies: veggies)
+#   {:ok, state}
+#  end
+#
+#  # test 7
+#  def handle_call(:fetch, _from, state) do
+#    {:reply, state, state}
+#  end
+#
+#  # test 8 
+#  def fetch(pid) do
+#    ret = :gen_server.call(pid, :fetch)
+#  end
+#
+#end
